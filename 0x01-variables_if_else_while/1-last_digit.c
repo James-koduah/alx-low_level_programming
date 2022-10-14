1e077c39dea151bb6084d0c422e9e8a0b80cd539
James-Koduah <jameskoduah171@gmail.com>
2022-10-14 13:25:40 -0700

    lasr

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	if (6 > last && 0 < last)
+	if (6 > last && 0 > last)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
