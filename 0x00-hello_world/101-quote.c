394f0d360d5f2dc953a39524f1bffe12244bfacc
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 06:48:18 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - lsls
+ * Return: lslls
+ */
+int main()
+{
+system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+return (1);
+}
+
