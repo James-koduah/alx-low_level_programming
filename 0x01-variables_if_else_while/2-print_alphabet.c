824d2946317577928339af835aa8b4f1871c9b3b
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 07:30:58 -0700

    22

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - lslsl
+ * Return: lslslsl
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
