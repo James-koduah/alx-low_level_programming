7312fedff8e347cfc200ce685b7f2b00899ca2de
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 10:09:32 -0700

    hope

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,17 @@
 int main(void)
 {
 	int ch;
-	int bb;
 
 	for (ch = '0' ; ch <= '9' ; ch++)
 	{
-		for (bb = '0' ; bb <= '9' ; bb++)
+		putchar(ch);
+		if (ch < '9')
 		{
-			putchar(ch);
-			putchar(bb);
-			putchar(',');
-		}	
-	
+		putchar(',');
+		putchar(' ');
+		}
 	}
+
 	putchar('\n');
 	return (0);
 }
