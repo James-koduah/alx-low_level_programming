7a9430e3254760a7a69a3d51552e5dfc457696f5
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 10:02:05 -0700

    lsls

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - jsjsj
+ * Return: lslsl
+ */
+int main(void)
+{
+	int ch;
+	int bb;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		for (bb = '0' ; bb <= '9' ; bb++)
+		{
+			putchar(ch);
+			putchar(bb);
+			putchar(',');
+		}	
+	
+	}
+	putchar('\n');
+	return (0);
+}
