61554dd992ff3cc7b242fe92fae37afbdc75305e
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 09:36:14 -0700

    first

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - lsllsl
+ * Return: ksksksk
+ */
+int main(void)
+{
+	int ch;
+	int bb;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (bb = 'a' ; bb <= 'f' ; bb++)
+	{
+		putchar(bb);
+	}
+	putchar('\n');
+	return (0);
+}
