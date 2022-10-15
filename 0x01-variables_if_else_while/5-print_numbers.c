08e82366a0509fb01605a505bc9075be058bf8b1
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 08:26:48 -0700

    lsl

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - kskdkd
+ * Return: ksksks
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10 ; ch++)
+	{
+		printf("%d", ch);
+	}
+	putchar('\n');
+	return (0);
+}
