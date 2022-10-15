ade8dad23087236fffeea2ecaa1463ca767c5d15
James-Koduah <jameskoduah171@gmail.com>
2022-10-15 08:09:58 -0700

    hope

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - lslsll
+ * Return: lslsls
+ */
+int main(void)
+{
+	int ch;
+	int bb;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (bb = 'A' ; bb <= 'Z' ; bb++)
+	{
+		putchar(bb);
+	}
+	putchar('\n');
+	return (0);
+}
