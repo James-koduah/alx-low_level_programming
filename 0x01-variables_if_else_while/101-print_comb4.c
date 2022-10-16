fe0a33a7a8c0ba68e1009bf3d4c3ac528ea9bb9b
James-Koduah <jameskoduah171@gmail.com>
2022-10-16 14:24:48 -0700

    whooho

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - lslslsl
+ * Return: kskskks
+ */
+int main(void)
+{
+	int ch;
+	int bb;
+	int cc;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		for (bb = ch + 1 ; bb <= '9' ; bb++)
+		{
+			for (cc = ch + 2 ; cc <= '9' ; cc++)
+			{
+				if (!(ch == bb) && !(bb == cc))
+				{
+					putchar(ch);
+					putchar(bb);
+					putchar(cc);
+					if (ch == '7' && bb == '8' && cc == '9')
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
