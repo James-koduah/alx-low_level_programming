063b509a380c2912c40b1500945cb45f12d97d9e
James-Koduah <jameskoduah171@gmail.com>
2022-10-17 02:09:07 -0700

    gg

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - lskdjdj
+ * Return: kdkdk
+ */
+int main(void)
+{
+	int aa;
+	int bb;
+	int cc;
+	int dd;
+
+	for (aa = '0' ; aa <= '9'; aa++)
+	{
+		for (bb = '0' ; bb <= '9' ; bb++)
+		{
+			for (cc = aa ; cc <= '9' ; cc++)
+			{
+				for (dd = bb+1 ; dd <= '9' ; dd++)
+				{
+					putchar(aa);
+					putchar(bb);
+					putchar(' ');
+					putchar(cc);
+					putchar(dd);
+					if(aa == '9' && bb == '8')
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
