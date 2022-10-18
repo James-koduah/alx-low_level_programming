d7dc953fef5985fd5acafcffe20932091968288c
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 15:02:08 -0700

    finr

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * print_to_98 - jakek
+ * @n: kkdkdkd
+ * Return: kdkdkdk
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			pritnf("%d", i);
+		}
+	}
+	else
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+}
