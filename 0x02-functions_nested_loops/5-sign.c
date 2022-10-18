ed66490f821d952d766f473946a6edd74369ddc3
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 11:10:33 -0700

    hope

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * print_sign - print sign pos or neg
- * @n: kkdkdk
+ * @c: kdkdkdk
  * Return: kdkdkd
  */
+int _putchar(char c);
 int print_sign(int n)
 {
 	int ch;
@@ -12,7 +13,7 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		bb = '-';
-		ch = -1;		 
+		ch = -1;
 		_putchar(bb);
 		return (ch);
 	}
@@ -20,14 +21,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		bb = '+';
-		ch = 1;		 
+		ch = 1;
 		_putchar(bb);
 		return (ch);
 	}
 	else
 	{
 		bb = '0';
-		ch = 0;		
+		ch = 0;
 		_putchar(bb);
 		return (ch);
 	}
