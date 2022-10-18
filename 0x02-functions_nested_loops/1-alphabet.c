1a8c6dce0587a7618fc25ab69a2cf24cbf1284f5
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 04:49:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - dddddd
+ * Return: ksslsl
+ */
+int print_alphabet();
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+int print_alphabet()
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
