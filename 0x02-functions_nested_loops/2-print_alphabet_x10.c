29a635aa3f5f1c76fc77c206cd78754cd89b6863
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 08:59:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - jskskks
+ * Return: alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while (i < 11)
+	{
+		int ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
+}
