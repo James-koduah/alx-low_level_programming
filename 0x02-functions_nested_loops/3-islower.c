048dbca8d2d21fe5abfc3b66bb8f523fd66dcc18
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 10:01:05 -0700

    finrst

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _islower - kdkdkkd
+ * @c: kdkdkdk
+ * Return: kdkdkkd
+ */
+int _islower(int c)
+{
+	int vh;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		vh = 1;
+	}
+	else
+	{
+		vh = 0
+	}
+	return (vh);
+}
