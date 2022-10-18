3d0caebc57bacf0fd66bc6b92acc020a7cb6f6d1
James-Koduah <jameskoduah171@gmail.com>
2022-10-18 10:57:15 -0700

    first

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_last_digit - maisll
+ * @c: kdkdk
+ * Return: kskskks
+ */
+int print_last_digit(int c)
+{
+	int ch;
+
+	ch = c % 10;
+	putchar(ch);
+	return (ch);
+}
