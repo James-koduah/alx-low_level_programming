229cfeae19692d0f4fc73aa4d182e342c466705f
James-Koduah <jameskoduah171@gmail.com>
2022-10-19 08:36:56 -0700

    first_fabonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - kdkdkdkdk
+ * Return: skskkssk
+ */
+int main(void)
+{
+	int aa;
+	int bb;
+	int ii;
+	int cc;
+
+	printf("0");
+	aa = 1;
+	bb = 0;
+	for (ii = 0; ii <= 50; ii++)
+	{
+		printf(", ");
+		printf("%d", aa);
+		cc = bb + aa;
+		bb = aa;
+		aa = cc;
+	}
+	return (0);
+}
