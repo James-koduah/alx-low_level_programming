03d6adf2abfb58fb4dd47594816402ca202f6b87
James-Koduah <jameskoduah171@gmail.com>
2022-10-19 09:08:57 -0700

    first_Liber_Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - kdkdkdkdk
+ * Return: skskkssk
+ */
+int main(void)
+{
+	long int aa;
+	long int bb;
+	int ii;
+	long int cc;
+	long int dd;
+
+	aa = 1;
+	bb = 1;
+	for (ii = 0; ii < 50; ii++)
+	{
+		cc = bb + aa;
+		bb = aa;
+		aa = cc;
+		if ((cc % 2) == 0)
+		{
+			dd += cc
+		}
+	}
+	printf("%ld", dd);
+	putchar('\n');
+	return (0);
+}
