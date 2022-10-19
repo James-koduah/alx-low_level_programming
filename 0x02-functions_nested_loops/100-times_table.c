b6efeca0a35d3e7426a8f35d7a3307ba48a92ee1
James-Koduah <jameskoduah171@gmail.com>
2022-10-19 07:27:55 -0700

    first100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *  print_times_table - kdkdkdk
+ *  @n: dkdkdk
+ * Return: kdkdk
+ */
+void print_times_table(int n)
+{
+	int ch;
+	int ii;
+	int jj;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (ch = 0; ch <= n; ch++)
+	{
+		putchar('0');
+		for (ii = 1; ii <= n; ii++)
+		{
+			jj = ii * ch;
+			putchar(',');
+			putchar(' ');
+			if (jj <= 9)
+			{
+				putchar(' ');
+			}
+			printf("%d", jj);
+		}
+		printf("\n");
+	}
+}
