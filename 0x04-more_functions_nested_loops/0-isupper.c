b1bcb641f0d22ddfc8b0f79de45109960f595faf
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 08:21:39 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isupper- ksksk
+ * @c: kdkdkdk
+ * Return: kdkdkdk
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_number(int n);
 int _putchar(char c);
+int _isupper(int c);
