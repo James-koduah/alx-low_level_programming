ca60f0086d9cee4adde2d11160486ac766f225d7
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 09:14:08 -0700

    james

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_line - jdjdjdj
+ * @n: kdkdkdk
+ * Return: kdkdkdk
+ */
+void print_line(int n)
+{
+	int ch;
+
+	if (n > 0)
+	{
+		for (ch = 0; ch < n; ch++)
+		{
+			putchar('_');
+		}
+	}
+	putchar('\n');
+}
