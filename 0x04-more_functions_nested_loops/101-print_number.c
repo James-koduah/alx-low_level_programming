c41244b78138b54e367864be16d6dab7237c9f89
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 07:01:15 -0700

    fuc

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,16 @@
 int _putchar(char c);
 void print_number(int n)
 {
-	int num;
+	unsigned int num = n;
 
-	num = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
-	
+
 	_putchar((num % 10) + '0');
 }
