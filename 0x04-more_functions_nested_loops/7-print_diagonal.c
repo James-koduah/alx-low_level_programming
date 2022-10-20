67b8d4980279d2db5f7961db230437d44b1e3f56
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 13:47:04 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * print_diagonal - jkkksks
+ * @n: kdkdkdk
+ * Return: kdkdkd
+ */
+void print_diagonal(int n)
+{
+	int ii;
+	int tt;
+	int bb;
+
+	tt = 0;
+	if (n > 0)
+	{
+		for (ii = 0; ii < n; ii++)
+		{
+			for (bb = 0; bb < tt; bb++)
+			{
+				putchar(' ');
+			}
+			tt++;
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
