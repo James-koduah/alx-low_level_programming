c27358679cd6fca29d0ebb7909c330bec7bf02e1
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 08:28:47 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - ksksk
+ * @a: kdkkddk
+ * @b: kddkkdkd
+ * Return: dkdkdk
+ */
+int mul(int a, int b)
+{
+	int bb;
+
+	bb = a * b;
+	return (bb);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ void print_number(int n);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
