718c1200b463f1d5ff4014a9ba38148375aaeab2
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 09:00:43 -0700

    ssssdew

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * more_numbers - jkjjjj
+ * Return: kdkdkd
+ */
+void more_numbers(void)
+{
+	int ii;
+	int ten;
+	int vv;
+
+	for (ten = 0; ten < 10; ten++)
+	{
+		for (ii = 0; ii <= 9; ii++)
+		{
+			putchar(ii);
+		}
+		for (vv = 0; vv < 5; vv++)
+		{
+			putchar('1');
+			putchar(vv);
+			printf('\n');
+		}
+
+	}
+}
