9af846ee5b3808e09939aec9d723e9aaad66a5f9
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 06:57:09 -0700

    ksksk

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,17 @@
 int _putchar(char c);
 void print_number(int n)
 {
+	int num;
+
+	num = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n / 10)
+	if ((num / 10) > 0)
 	{
-		main(n / 10);
+		print_number(num / 10);
 	}
-		_putchar((n % 10) + '0');
+		_putchar((num % 10) + '0');
 }
