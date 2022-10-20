b95649b478bda9d43afeb93fa195fd5154c10e53
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 08:32:04 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_numbers - dkkdkd
+ * Return: kskskksk
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
