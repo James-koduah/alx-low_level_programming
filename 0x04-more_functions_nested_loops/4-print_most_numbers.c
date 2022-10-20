19c9a2c4547b70cdec0abdd2bf529ff26353e0b9
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 08:35:13 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_numbers - dkkdkd
+ * Return: kskskksk
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (ch != '2' || ch != '4')
+		{
+		putchar(ch);
+		}
+	}
+	putchar('\n');
+}
