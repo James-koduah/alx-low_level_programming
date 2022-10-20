0e454c814dc27b4c52d16847f7f89db7cece4714
James-Koduah <jameskoduah171@gmail.com>
2022-10-20 08:45:43 -0700

    badCode

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,18 @@
  * print_most_numbers - dkkdkd
  * Return: kskskksk
  */
-void print_most_numbers(void)
+void main(void)
 {
 	int ch;
 
 	for (ch = '0'; ch <= '9'; ch++)
 	{
-		if (ch != '2' || ch != '4')
+		if (ch == '2' || ch == '4')
+		{	
+		}
+		else
 		{
-		putchar(ch);
+			putchar(ch);
 		}
 	}
 	putchar('\n');
