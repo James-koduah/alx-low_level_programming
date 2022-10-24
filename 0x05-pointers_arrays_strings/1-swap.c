1d91f830db246fddbc0940230716271afbb19485
James-Koduah <jameskoduah171@gmail.com>
2022-10-24 09:25:46 -0700

    first1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap_int - swap two numbers
+ * @a: ksksk
+ * @b: kdkdkdk
+ * Return: kdkdkk
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	*b = c;
+	*b = d;
+}
