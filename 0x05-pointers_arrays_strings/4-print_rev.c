592811c22dca73de62228e6e6d0a1bd7054fab38
James-Koduah <jameskoduah171@gmail.com>
2022-10-24 10:54:36 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_rev - print a string form a pointed position
+ * @s: kskskks
+ * Return: lskdkdk
+ */
+void print_rev(char *s)
+{
+	int jj;
+
+	jj = 0;
+	while (*s != 0)
+	{
+		s++;
+		jj++;
+	}
+
+	while (jj >= 0)
+	{
+		putchar(*s);
+		s--;
+	}
+	putchar('\n');
+}
