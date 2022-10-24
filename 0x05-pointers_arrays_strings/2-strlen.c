f555ab967c7cc13a7f08cb684bf7496fdfef2155
James-Koduah <jameskoduah171@gmail.com>
2022-10-24 10:01:13 -0700

    strlen-first

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - length of string
+ * @s: ksksk
+ * Return: kdkdkdk
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
