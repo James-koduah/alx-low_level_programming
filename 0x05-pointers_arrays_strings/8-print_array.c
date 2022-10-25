7a68d6909a691a09d96db7ceafebe4dd0af65888
James-Koduah <jameskoduah171@gmail.com>
2022-10-25 07:14:33 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_array - lsslls
+ * @a: the array
+ * @n: the number of elements to print
+ * Return: kdkdkd
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{	
+		printf("%d",a[i]);
+	}	
+}
