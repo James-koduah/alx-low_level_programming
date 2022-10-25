cf9f7caa0af12d01e50c013c8acd3e56af5543c0
James-Koduah <jameskoduah171@gmail.com>
2022-10-25 11:07:27 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - kskskks
+ * @dest: kkdkdkkd
+ * @src: kdkkddkk
+ * Return: kdkkdkdk
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest,src);
+	return (dest);
+}
