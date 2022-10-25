545ba76d0deee75232cd60b7b29fe3d13e3768a1
James-Koduah <jameskoduah171@gmail.com>
2022-10-25 13:47:08 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,20 +7,20 @@
 */
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	int  = 1;
+	unsigned int b = 0;
 
 	do {
 		if (*s == '-')
-			sign *= -1;
+			a *= -1;
 
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
+			b = (b * 10) + (*s - '0');
 
-		else if (num > 0)
+		else if (b > 0)
 			break;
 
 	} while (*s++);
 
-	return (num * sign);
+	return (b * a);
 }
