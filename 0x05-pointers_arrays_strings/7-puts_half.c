1d34ed9d823accad2b713e571245ddfb9faed543
James-Koduah <jameskoduah171@gmail.com>
2022-10-25 06:15:48 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - print second hslf of string
+ * @str: kksksk
+ * Return: kskskks
+ */
+void puts_half(char *str)
+{
+	int len;
+	int mid;
+	int i;
+	int n;
+	
+	i = 0;
+	while (str)
+	{
+		i++;
+		str++;
+	}
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+	}
+	else
+	{
+		n = (i - 1) / 2;
+	}
+	str = str - n;
+	while (str)
+	{
+		putchar(str);
+		str++;
+	}
+}
