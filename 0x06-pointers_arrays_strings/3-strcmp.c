3123d135a4c979bde04a4943257a7975ea50854a
James-Koduah <jameskoduah171@gmail.com>
2022-10-26 04:45:13 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - kslsks
+ * @s1: ksskks
+ * @s2: sksksk
+ * Return: kskskks
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int ch;
+
+	ch = strcmp(s1, s2);
+	return (ch);
+}
