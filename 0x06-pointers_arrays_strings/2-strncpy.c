f260e4c80d81638827df33c601852f90906b7674
James-Koduah <jameskoduah171@gmail.com>
2022-10-26 04:38:59 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - copy
+ * @dest: kdkdkdk
+ * @src: kdkdkdk
+ * @n: kskskks
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (*dest);
+}
