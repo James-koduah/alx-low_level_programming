3e35a8637581104d6af00427e4f23042d11568c8
James-Koduah <jameskoduah171@gmail.com>
2022-10-31 12:12:07 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strspn - kskskks
+ * @s: kdkdkdk
+ * @accept: kskskksks
+ * Return: kskksks
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int ch;
+
+	ch = strspn(s, accept);
+	return (ch);
+}
