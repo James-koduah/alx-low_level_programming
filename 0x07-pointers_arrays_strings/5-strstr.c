25fe0edf4a05b3fc927ec9623605f2902c45f0b6
James-Koduah <jameskoduah171@gmail.com>
2022-10-31 12:42:32 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr - skksks
+ * @haystack: kskskks
+ * @needle: kskskksks
+ * Return: skskskks
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *men;
+
+	men = strstr(haystack, needle);
+	return (men);
+}
