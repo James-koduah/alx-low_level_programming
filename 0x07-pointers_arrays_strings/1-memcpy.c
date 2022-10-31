8781b7d61b7a28630834f779396d03466ec197ab
James-Koduah <jameskoduah171@gmail.com>
2022-10-31 06:57:58 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memcpy - kskskk
+ * @dest: ksksksk
+ * @src: ksksksk
+ * @n: kskksks
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, sizeof(char) * n);
+
+	return (dest);
+}
