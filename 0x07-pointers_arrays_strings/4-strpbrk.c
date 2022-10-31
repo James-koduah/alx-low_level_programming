3436256e162bf7db7d49d6d82a7e1ebbda820ec8
James-Koduah <jameskoduah171@gmail.com>
2022-10-31 12:24:13 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - kskksks
+ * @s: ksksksksk
+ * @accept: kskskks
+ * Return: kskksk
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *mat;
+
+	mat = strpbrk(s, accept);
+	return (mat);
+}
