44629f1cdd93b5c9b6bbab2ec308cabd4adfbb34
James-Koduah <jameskoduah171@gmail.com>
2022-11-01 03:20:57 -0700

    diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_diagsums - kskksksk
+ * @a: kskskksks
+ * @size: kskksks
+ * Return: kskksksk
+ */
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int vv;
+	
+
+	vv = 0;
+	for (b = 0;b < size; b++)
+	{
+		vv += a[b];
+		a += size;
+	}
+	printf("%d, ", vv);
+	a -= size;
+	vv = 0;
+	for (b = 0; b < size; b++)
+	{
+		vv += a[b];
+		a -= size;
+	}
+	printf("%d\n", vv);
+
+}
