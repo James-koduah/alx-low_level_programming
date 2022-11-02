7d1b2a8104033d813d3c8fb193d053387bd4e7e1
James-Koduah <jameskoduah171@gmail.com>
2022-11-02 07:20:59 -0700

    3-main.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * factorial - kksksksk
+ * @n: kskksksk
+ * Return: kskskksk
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
