4f198b3263d264e96012599da1fbd85f77c1f6fe
James-Koduah <jameskoduah171@gmail.com>
2022-11-02 05:17:55 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - sksjksksk
+ * @s: kskksksks
+ * Return: ksksksksk
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+	
+	n = 0;
+	if (*s != 0)
+	{
+		n = 1;
+		n += _strlen_recursion(s + 1);
+	}
+	return (n);
+}
