ef7509b7f2a7d8ce0476dded59f0cc5c5a1a78e2
James-Koduah <jameskoduah171@gmail.com>
2022-11-04 05:09:19 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - kskks
+ * @argc: ksksk
+ * @argv: skksk
+ * Return: kskks
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n",i * b);
+
+
+	return 0;
+}
