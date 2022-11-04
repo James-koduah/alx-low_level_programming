954cf5f8e85b2524a8610e7ba10f431bd0f52983
James-Koduah <jameskoduah171@gmail.com>
2022-11-04 04:52:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - kskks
+ * @argc: sksksk
+ * @argv: ksksksk
+ * Return: ksksk
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return 0;
+}
