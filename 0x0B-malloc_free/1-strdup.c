bf01b168a5aabbc6e29996ced264c2cbc862ab37
James-Koduah <jameskoduah171@gmail.com>
2022-11-07 06:30:24 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - kskksk
+ * @str: ksksksk
+ * Return: ksksk
+ */
+char *_strdup(char *str)
+{
+	char *b;
+	if (str == NULL)
+	{
+	       return (NULL);
+	}
+
+	b = malloc(sizeof(char) * 10);
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(b, str);
+	return (b);		
+}
