1016987a27ccd9da22dff34435c43b794c2c8061
James-Koduah <jameskoduah171@gmail.com>
2022-11-07 09:18:26 -0800

    third

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,8 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+			return (NULL);
 	}
 	for (b = 0; b < height; b++)
 	{
