1abd26fa1643c2be6e3da0b7bc77745687fdd8f2
James-Koduah <jameskoduah171@gmail.com>
2022-11-07 07:27:08 -0800

    third

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,23 @@ char *str_concat(char *s1, char *s2)
 	int v;
 	int a;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	v = strlen(s1);
 	a = strlen(s2);
 	c = a + v;
-	b = malloc(c);
+	b = malloc(c + 1);
+	if (b == NULL)
+	{
+		return (NULL);
+	}
 	for (c = 0; c < v; c++)
 	{
 		b[c] = s1[c];
