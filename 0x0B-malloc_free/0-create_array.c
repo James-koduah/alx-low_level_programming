8e3cf4f72f21ffd36b554798a79a9382bafdfd48
James-Koduah <jameskoduah171@gmail.com>
2022-11-07 04:56:08 -0800

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - allocates memory
+ * @size: create_arrayksks
+ * @c: kskks
+ * Return: kskskks
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	
+
+	p = malloc(size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+
+}
