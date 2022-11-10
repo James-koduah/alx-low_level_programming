64806a36b529a3a506198e01916c9c196d6696ee
James-Koduah <jameskoduah171@gmail.com>
2022-11-10 06:44:26 -0800

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,20 +9,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
-	unsigned int i;
+
 
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
-		return (NULL);
-	
-	for (i = 0; i < nmemb; i++)
-	{
-		arr[i] = '\0';
-	}
+	arr = calloc(nmemb, size);	
 	return (arr);
 
 }
