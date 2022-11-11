3842e643ab5e1f468d7c1b1c3643a122fe96bdbb
James-Koduah <jameskoduah171@gmail.com>
2022-11-11 05:43:04 -0800

    third

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,6 +10,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (old_size == new_size)
 		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	return (ptr);
 }
