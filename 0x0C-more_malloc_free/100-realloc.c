1f4e0a4313bea908a6b57e7ef91e440ead837497
James-Koduah <jameskoduah171@gmail.com>
2022-11-11 05:37:45 -0800

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**_realloc - imitate realloc
+ * @ptr: kksksk
+ * @old_size: kskksk
+ * @new_size: kskskk
+ * Return: skskk
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (old_size == new_size)
+		return (ptr);
+
+
+}
