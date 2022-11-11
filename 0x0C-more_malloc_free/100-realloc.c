35854369fb6cdfc7727dd3cf2f0cbc083c1df1eb
James-Koduah <jameskoduah171@gmail.com>
2022-11-11 11:44:38 -0800

    test2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -37,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 	}
 	fill_mem = arr;
-	for (i = 0; i < old_size && i < new_size; i++)
+	for (i = 0; i < old_size; i++)
 	{
 		fill_mem[i] = *ptr_new++;
 	}
