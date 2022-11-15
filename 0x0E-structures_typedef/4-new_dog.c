578ee05276db4cfea90f6a60f3eba32238f0ed1a
James-Koduah <jameskoduah171@gmail.com>
2022-11-15 07:12:49 -0800

    third

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	aa = strlen(name);
-	bb->name = malloc(sizeof(char) * aa);
+	bb->name = malloc(sizeof(char) * aa + 1);
 	if (bb->name == NULL)
 	{
 		free(bb->name);
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	bb->age = age;
 
 	aa = strlen(owner);
-	bb->owner = malloc(sizeof(char) * aa);
+	bb->owner = malloc(sizeof(char) * aa + 1);
 	if (bb->owner == NULL)
 	{
 		free(bb->owner);
