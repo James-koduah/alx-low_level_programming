64f4478b250be00909f9546e6548005b4d778fd6
James-Koduah <jameskoduah171@gmail.com>
2022-11-16 09:47:28 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * int_index - labalaba
+ * @array: ksksksk
+ * @size: ksksksk
+ * @cmp: kskskks
+ * Return: kskksk
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int c;
+
+	if (size <= 0)
+		return (-1);
+
+	c = 0;
+	for (i = 0; i < size; i++)
+	{
+		c = cmp(array[i]);
+		if (c != 0)
+			return (i);
+	}
+	return (-1);
+}
