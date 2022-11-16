a50a34f226a832095d9ba76a794d5d15f75e6b60
James-Koduah <jameskoduah171@gmail.com>
2022-11-16 12:58:18 -0800

    task_3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - kdkdk
+ * @a: kkskks
+ * @b: ksksksk
+ * Return: addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - kdkdk
+ * @a: kkskks
+ * @b: ksksksk
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - kdkdk
+ * @a: kkskks
+ * @b: ksksksk
+ * Return: multiply
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - kdkdk
+ * @a: kkskks
+ * @b: ksksksk
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - kdkdk
+ * @a: kkskks
+ * @b: ksksksk
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % d);
+}
