d32d9696315d08beb3f33e32ed6f129e8e26cb9d
James-Koduah <jameskoduah171@gmail.com>
2022-11-16 08:32:19 -0800

    second

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
-
+	
+	if (array == NULL || action == NULL || size == 0)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
