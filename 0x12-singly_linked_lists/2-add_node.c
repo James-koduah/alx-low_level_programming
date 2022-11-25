1b0ddeef7eb40d8e7f768cdf84a633318e4fab9c
James-Koduah <jameskoduah171@gmail.com>
2022-11-25 03:06:02 -0800

    2-add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - add  a new node at the begining of the list
+ * @head: list
+ * @str: ksksk
+ * Return: ksksksk
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
