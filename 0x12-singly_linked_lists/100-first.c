77478fd504729890f468bea59dbe87d2fd703b00
James-Koduah <jameskoduah171@gmail.com>
2022-11-26 09:24:58 -0800

    tortise_and_hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * prin - ksksksk
+ * Return: ksksk
+ */
+void prin() __attribute__((constructor));
+void prin()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
