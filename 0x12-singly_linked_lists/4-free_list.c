518847ce463352c20404962916d84be22bd938e0
James-Koduah <jameskoduah171@gmail.com>
2022-11-26 09:11:40 -0800

    4-main.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,27 +9,12 @@ void free_list(list_t *head)
 {
 	list_t *h;
 
-	if (head == NULL)
-	{
-		free(head->str);
-		free(head);
-	}
-	else
-	{
-	while (head->next != NULL)
+	while (head)
 	{
 		h = head->next;
 		free(head->str);
+		free(head->next);
 		free(head);
 		head = h;
-	}
-	/*
-	 * The last link is not cleared
-	 * because it's next node is NULL
-	 * so the loop will stop at the last
-	 * one hence this removal
-	*/
-	free(head->str);
-	free(head);
 	}
 }
