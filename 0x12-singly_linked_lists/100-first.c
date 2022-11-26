2fa7240b953e84dbc0debd3ef43e64d1d6c1612a
James-Koduah <jameskoduah171@gmail.com>
2022-11-26 09:26:22 -0800

    second

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,9 @@
  * prin - ksksksk
  * Return: ksksk
  */
-void prin() __attribute__((constructor));
-void prin()
+void prin(void) __attribute__((constructor));
+void prin(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
