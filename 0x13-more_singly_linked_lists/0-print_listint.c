f948a8442d01f91501d2123a416c23cc323c3e68
James-Koduah <jameskoduah171@gmail.com>
2022-11-28 12:17:52 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - print the interger in each node
+ * @h: header of list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int a;
+
+	a = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		a++;
+		h = h->next;
+	}
+	return (a);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,9 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,3 +17,8 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+
+
+size_t print_listint(const listint_t *h);
+#endif
