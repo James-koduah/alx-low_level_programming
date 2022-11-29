6b9867c5434dda1373903c7e09992d1efb060359
James-Koduah <jameskoduah171@gmail.com>
2022-11-29 04:57:50 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free lists
+ * @head: head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *h;
+	listint_t *g;
+
+	h = *head;
+
+	while (h)
+	{
+		g = h->next;
+		free(h);
+		h = g;
+	}
+	*head = NULL;
+}
