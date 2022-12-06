63f8fb728184492c8c1e78bc11b3ff2ebe18846e
James-Koduah <jameskoduah171@gmail.com>
2022-12-06 02:25:55 -0800

    second commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		w = write(o, text_content, strlen(text_content));
-	if (w == -1)
+	if (w == -1 || w == 13)
 		return (-1);
 
 	close(o);
