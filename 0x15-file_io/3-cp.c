aa2daeb8d4f503821e5da6efb324452c6cd9a720
James-Koduah <jameskoduah171@gmail.com>
2022-12-07 02:20:54 -0800

    3-cp.c first commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+
+int main(int ac, char *av[])
+{
+
+	int f1_open, f1_read, f1_write;
+	int f2_open, f2_read, f2_write;
+	char *buf;
+
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		return (-1);
+	if (ac != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+
+	f1_open = open(av[1], O_RDONLY);
+	if (f1_open == -1)
+	{
+		printf("Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	f2_open = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (f2_open == -1)
+	{
+		printf("Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	f1_read = 1;
+	while (f1_read > 0)
+	{
+		f1_read = read(f1_open, buf, 1023);
+		f2_write = write(f2_open, buf, f1_read);
+	}
+
+	free(buf);
+	return (0);
+}
