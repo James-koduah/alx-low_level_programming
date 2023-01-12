073263ab71b37298947a3aa74a748b2c2e0ea7ef
James-Koduah <jameskoduah171@gmail.com>
2023-01-12 13:57:51 -0800

    second draft 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,12 +6,7 @@ size_t print_dlistint(const dlistint_t *h)
 	int i = 0;
 
 
-	while (h->prev != NULL)
-	{
-		h = h->prev;
-	}
-
-	while (h->next != NULL)
+	while (h)
 	{
 		i++;
 		printf("%d\n", h->n);
