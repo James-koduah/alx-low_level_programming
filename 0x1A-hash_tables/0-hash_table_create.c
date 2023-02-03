3dfa7f651dd0221bc30e0c2fa00e5d94ddf85817
James-Koduah <jameskoduah171@gmail.com>
2023-02-03 11:25:46 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t*));
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
