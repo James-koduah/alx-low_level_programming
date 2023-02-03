ac6849a8d26ecdfa54f702ddc996fff7854cddf1
James-Koduah <jameskoduah171@gmail.com>
2023-02-03 11:16:16 -0800

    0-hash_table_create.c 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table
+ * @size: size of the hash table
+ * Return: A hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t));
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return table;
+}
