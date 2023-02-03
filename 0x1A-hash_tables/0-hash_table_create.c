474a6fda4b94558fe3f49144bfd3f7a79c2bb04e
James-Koduah <jameskoduah171@gmail.com>
2023-02-03 11:37:35 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
+	hash_table_t *table;
+       
+	table =	malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t*));
+	table->array = malloc(size * sizeof(hash_node_t*));
+	if (table->array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
