fbfc450c20069cfb00999d17ad37747d5ac432fa
James-Koduah <jameskoduah171@gmail.com>
2023-02-05 12:14:33 -0800

    sorry

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node = malloc(sizeof(hash_node_t));
 
-	if (key == NULL || ht == NULL || node == NULL || value == NULL || *key = '\0')
+	if (key == NULL || ht == NULL || node == NULL || value == NULL || *key == '\0')
 		return (0);
 
 	node->key = strdup(key);
