3f51dc1d2c877aecab1ccd79563892d5e4971d21
James Koduah <jameskoduah171@gmail.com>
2023-05-19 17:20:16 +0000

    1-binary: betty corrections

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 void print_array(int *array, size_t l, size_t r);
+
+/**
+ * binary_search - a search algorithm with binary search
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: The value we are looking for
+ * Return: the index of the value or -1 if we cannot find it
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0;
@@ -20,10 +28,17 @@ int binary_search(int *array, size_t size, int value)
 	return (-1);
 }
 
+/**
+ * print_array - Used to print out which section of the array we are working on
+ * @array: the array
+ * @l: the left partition
+ * @r: the right partition
+ * Return: void
+ */
 void print_array(int *array, size_t l, size_t r)
 {
 	size_t i;
-	
+
 	printf("Searching in array:");
 	for (i = l; i < r; i++)
 	{
