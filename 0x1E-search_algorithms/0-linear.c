09647ae776ec87b5518d6cd486e4771806ab45ce
James Koduah <jameskoduah171@gmail.com>
2023-05-19 14:29:03 +0000

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	for (i = 0; i < size; i++ )
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
