4c58876eae8fa5e475670537bf476d55b9e7e445
James Koduah <jameskoduah171@gmail.com>
2023-07-10 13:05:40 +0000

    complexity files

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SER__
+#define __SER__
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+#endif
