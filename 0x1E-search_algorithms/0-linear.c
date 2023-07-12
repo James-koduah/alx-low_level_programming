d1193365b2e9ec3cc78ac9cc541e5883761eab4f
James Koduah <jameskoduah171@gmail.com>
2023-07-12 22:14:51 +0000

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
@@ -22,5 +22,5 @@ int linear_search(int *array, size_t size, int value)
 			return (i);
 		}
 	}
-	return (-1); 
+	return (-1);
 }
